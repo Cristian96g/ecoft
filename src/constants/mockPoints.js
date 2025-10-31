76df4d8827a832fc7554d5a1745318b791b0c1c9
Cristian Gomez <cristian.gomez.r1089@gmail.com>
2025-10-31 00:25:04 -0300

    Actualizando datos calendar

--- src/constants/mockPoints.js
+++ src/constants/mockPoints.js
@@ -2,21 +2,39 @@ const mockPoints = [
   {
     _id: 1,
     name: "Punto Verde - Centro",
-    location: { type: "Point", coordinates: [-72.264, -50.340] }, // lng, lat
-    types: ["plastico", "vidrio"]
+    barrio: "Centro",
+    estado: "activo",
+    address: "Av. Libertador 1100",
+    location: { type: "Point", coordinates: [-72.264, -50.340] }, // [lng, lat]
+    types: ["plastico", "vidrio"],
   },
   {
     _id: 2,
     name: "Punto Verde - Lago Argentino",
+    barrio: "Lago Argentino",
+    estado: "activo",
+    address: "Costanera y Los Pioneros",
     location: { type: "Point", coordinates: [-72.275, -50.345] },
-    types: ["papel", "pilas"]
+    types: ["papel", "pilas"],
   },
   {
     _id: 3,
     name: "Punto Verde - Aeropuerto Viejo",
+    barrio: "Aeropuerto Viejo",
+    estado: "inactivo",
+    address: "Av. del Libertador 3200",
     location: { type: "Point", coordinates: [-72.255, -50.337] },
-    types: ["aceite", "plastico"]
-  }
+    types: ["aceite", "plastico"],
+  },
+  {
+    _id: 4,
+    name: "Punto Verde - Punta Soberana",
+    barrio: "Punta Soberana",
+    estado: "activo",
+    address: "Calle 25 y Los Álamos",
+    location: { type: "Point", coordinates: [-72.300, -50.355] },
+    types: ["vidrio", "papel"],
+  },
 ];
 
 export default mockPoints;
